5c8a0958714f9102ead3abc258906cf854b3d829
Tim Kosem <tim.kosem@ey.com>
2018-02-01 16:27:13 -0500

    moved todo-actions to store folder

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IAppState, rootReducer, INITIAL_STATE } from './store/store';
-import { TodoActions } from './todo-actions'
+import { TodoActions } from './store/todo-actions'
 
 
 import { AppComponent } from './app.component';
